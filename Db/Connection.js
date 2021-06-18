6976989c555859132544cb09ac3a1f911f6a6bc1
muneebulislam <muneebulislam@hotmail.com>
2021-06-18 01:12:05 -0600

    removed some values

--- Db/Connection.js
+++ Db/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = "mongodb+srv://dbUser:testDb123@cluster0.taxq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URI = "copy uri";
 const connectDb = async() => {
     await mongoose.connect(URI,{ useUnifiedTopology: true, useNewUrlParser: true});
     console.log('Your database has been connected');
